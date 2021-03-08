ea789bd3cfe7f0fab91113acdb1e16f8eba7184e
negative1skunk@gmail.com <silaslowe@gmail.com>
2021-03-08 12:49:20 -0600

    models added and migrated

--- readAloudapi/models/profile.py
+++ readAloudapi/models/profile.py
@@ -1,8 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.db.models.fields.files import ImageField
 
-class Profile(models.model):
+class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     role = models.CharField(max_length=25)
     bio = models.CharField(max_length=500)
--- readAloudapi/models/question.py
+++ readAloudapi/models/question.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-class Question(models.model):
+class Question(models.Model):
     book = models.ForeignKey("Book", on_delete=models.CASCADE)
     question = models.CharField(max_length=150)
     page = models.CharField(max_length=3)
