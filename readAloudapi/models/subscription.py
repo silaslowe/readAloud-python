5cb44adb8e1f1c1f81d753601957133219881855
negative1skunk@gmail.com <silaslowe@gmail.com>
2021-03-08 15:17:53 -0600

    added all fixtures except subscribe

--- readAloudapi/models/subscription.py
+++ readAloudapi/models/subscription.py
@@ -3,5 +3,5 @@ from django.contrib.auth.models import User
 
 
 class Subscription(models.Model):
-    profile = models.ForeignKey("Profile", on_delete=models.CASCADE)
+    profile = models.ForeignKey("Profile", related_name="subcriber", on_delete=models.CASCADE)
     user = models.ForeignKey("Profile", related_name="subscribee", on_delete=models.CASCADE)
