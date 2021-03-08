51387a966ada261d70e23e6df4b255c616c595c9
negative1skunk@gmail.com <silaslowe@gmail.com>
2021-03-08 16:03:49 -0600

    html erro on loogin in postman

--- readAloud/urls.py
+++ readAloud/urls.py
@@ -13,9 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
+from django.conf.urls import include
 from django.urls import path
+from readAloudapi.views import register_user, login_user
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+# Requests to http://localhost:8000/register will be routed to the register_user function
+    path('register', register_user),
+# Requests to http://localhost:8000/login will be routed to the login_user function
+    path('login', login_user),
+    path('api-auth', include('rest_framework.urls', namespace='rest_framework'))
 ]
