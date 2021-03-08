4dd35f7377fc5e3876ca7bb1c80c507af33999a1
negative1skunk@gmail.com <silaslowe@gmail.com>
2021-03-08 13:26:52 -0600

    migrations complete

--- readAloudapi/models/subscription.py
+++ readAloudapi/models/subscription.py
@@ -1,5 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 class Subscription(models.Model):
     profile = models.ForeignKey("Profile", on_delete=models.CASCADE)
-    user = models.ForeignKey("User", on_delete=models.CASCADE)
+    user = models.ForeignKey("Profile", related_name="subscribee", on_delete=models.CASCADE)
