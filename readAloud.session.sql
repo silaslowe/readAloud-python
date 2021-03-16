695ad6ba89fd530a640a4bdd06c94509b6d2ea65
negative1skunk@gmail.com <silaslowe@gmail.com>
2021-03-16 15:11:41 -0500

    book edit form now edits. fails on page reload

--- readAloud.session.sql
+++ readAloud.session.sql
@@ -8,4 +8,27 @@ WHERE id = 9;
 
 DELETE
 FROM readAloudapi_booktopic
-WHERE id = 5; 
+WHERE id = 5; INSERT INTO readAloudapi_book (
+    id,
+    title,
+    author,
+    publish_year,
+    notes,
+    cover_url,
+    rating,
+    location,
+    synopsis,
+    profile_id
+  )
+VALUES (
+    id:integer,
+    'title:varchar(75)',
+    'author:varchar(75)',
+    publish_year:integer,
+    'notes:text',
+    'cover_url:varchar(125)',
+    rating:real,
+    'location:varchar(50)',
+    'synopsis:text',
+    profile_id:integer
+  );
